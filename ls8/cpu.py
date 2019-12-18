96d806e2186b709cdda64f8e89fcdf6c5e188fcb
Hunter Raffety <hsraffety@gmail.com>
2019-12-18 13:40:35 -0700

    working on getting caught up, going into day2 lecture now

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -13,9 +13,17 @@
 00000001 # HLT
 '''
 
+LDI = 0b10000010 # LDI R0,8 "Load Immediately"
+NOP = 0b00000000 # "No Operation"
+# don't know what this is. 0b00001000
+PRN = 0b01000111 # PRN R0 "Print"
+# same as NOP 0b00000000
+HLT = 0b00000001 # HLT "Halt"
+
 """CPU functionality."""
 
 import sys
+print(sys.argv)
 
 class CPU:
     """Main CPU class."""
@@ -24,8 +32,7 @@ class CPU:
         """Construct a new CPU."""
         self.ram = {} #  our '256' bytes of memory storage
         self.reg = [0] * 8 # 8 general purpose registers
-        self.pc = 0
-        # pass
+        self.pc = 0 # program counter, index into memory of the current instruction
 
     def load(self):
         """Load a program into memory."""
@@ -36,12 +43,12 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010, # LDI R0,8 - 130
             0b00000000,
-            0b00001000,
+            0b00001000, # - 8
             0b01000111, # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001, # HLT - 1
         ]
 
         for instruction in program:
@@ -85,21 +92,37 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
+        running = True
+
+        while running:
+            instruction = self.ram[self.pc]
+            print(instruction)
         # Step 4: Implement the HLT instruction handler
         # Add the HLT instruction definition to cpu.py so that you can refer to it by name instead of by numeric value.
         # In run() in your switch, exit the loop if a HLT instruction is encountered, regardless of whether or not there are more lines of code in the LS-8 program you loaded.
+        if instruction is HLT:
+            running = False
         # We can consider HLT to be similar to Python's exit() in that we stop whatever we are doing, wherever we are.
         
         # Step 5: Add the LDI instruction
         # This instruction sets a specified register to a specified value.
-        # SeetheLS - 8spec for the details of what this instructions does and its opcode value.
-        
+        # See the LS-8 spec for the details of what this instruction does and its opcode value.
+        elif instruction is LDI:
+            register_number = self.reg[self.pc + 1]
+            self.reg[register_number] = self.ram[self.pc + 2]
+            self.pc += 3 # 3 steps PC ^^
+
         # Step 6: Add the PRN instruction
         # This is a very similar process to adding LDI, but the handler is simpler. See the LS-8 spec.
         # At this point, you should be able to run the program and have it print 8 to the console!
+        elif instruction is PRN:
+            idx = self.ram[self.pc + 1]
+            register_number = self.reg[self.pc + 1]
+            self.pc += 2 # 2 step PC ^^
 
-        pass
-
+        else:
+            print(f"Unknown instruction at index {self.pc}")
+            sys.exit(1)
 """
 base 10, decimal: 0-9
 base 2, binary: 0, 1
@@ -110,4 +133,6 @@ binary: 100001
 00000011 = 3
 00101010 = 42
 128, 64, 32, 16, 8, 4, 2, 1 etc.
+
+nybbles = 4bit blocks
 """
